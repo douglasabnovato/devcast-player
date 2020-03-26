882b511a7f0c955948c3bf8d6edd73cc5b11d369
@douglasabnovato <douglasabnovato@gmail.com>
2020-03-26 20:48:58 -0300

    tocar o próximo e atualizar detalhes

--- player.js
+++ player.js
@@ -7,18 +7,20 @@ window.player = {
     currentAudio: {},
     currentPlaying:0,
     start(){
+      this.update();
+      this.audio.onended = () => this.next();
+    },
+    next(){
+        this.currentPlaying++;
+        this.update();
+        this.audio.play();
+    },
+    update(){
+        this.currentAudio = this.audioData[this.currentPlaying];
 
-      this.currentAudio = this.audioData[this.currentPlaying];
-
-      this.cover.style.background = `url('${path(this.currentAudio.cover)}') no-repeat center center / cover` ;
-      this.title.innerText = this.currentAudio.title;
-      this.artist.innerHTML = `<i class='material-icons'>account_circle</i>${this.currentAudio.artist}`;
-      this.audio.src = path(this.currentAudio.file);
-
-      this.audio.addEventListener("ended", () => {
-          this.currentPlaying++;
-          this.audio.src = path(this.audioData[this.currentPlaying].file);
-          this.audio.play();
-      });
+        this.cover.style.background = `url('${path(this.currentAudio.cover)}') no-repeat center center / cover` ;
+        this.title.innerText = this.currentAudio.title;
+        this.artist.innerHTML = `<i class='material-icons'>account_circle</i>${this.currentAudio.artist}`;
+        this.audio.src = path(this.currentAudio.file);
     }
 };
